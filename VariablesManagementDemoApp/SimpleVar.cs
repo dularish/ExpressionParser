5016367ba4d564561ffb7e397ffa128b1870281a
kuttudla <kuttudla@schaeffler.com>
2019-08-12 10:42:41 +0200

    Added WPF Demo App

---
+++ VariablesManagementDemoApp/SimpleVar.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesManagementDemoApp
+{
+    public class SimpleVar : INotifyPropertyChanged
+    {
+        private string _strValue = string.Empty;
+        private string _evaluatedValue = string.Empty;
+        private DateTime _lastUpdatedDateTime = DateTime.Now;
+        private string _name;
+
+        public string StrValue
+        {
+            get
+            {
+                return _strValue;
+            }
+
+            set
+            {
+                if(_strValue != value)
+                {
+                    _strValue = value;
+                    onPropertyChanged();
+                    EvaluatedValue = evaluate(value);
+                    LastUpdatedDateTime = DateTime.Now;
+                    ValueUpdated?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        private ObservableCollection<SimpleVar> _centralizedVariablesCollection;
+
+        private string evaluate(string value)
+        {
+            Dictionary<string, string> dictForEvaluation = _centralizedVariablesCollection.Where(s => s != this).ToDictionary(s => s.Name, s => s.StrValue);
+            var parsedOutput = MathematicalExpressionParser.parseAndEvaluateExpression(value, dictForEvaluation);
+
+            if (parsedOutput.Item1.IsEvaluationSuccess)
+            {
+                return (parsedOutput.Item1 as MathematicalExpressionParser.ExpressionEvaluationResult.EvaluationSuccess).Item.ToString();
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public void Destroy()
+        {
+            ValueDestroyed?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void onPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string EvaluatedValue
+        {
+            get => _evaluatedValue; set
+            {
+                if(_evaluatedValue != value)
+                {
+                    _evaluatedValue = value;
+                    onPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime LastUpdatedDateTime
+        {
+            get => _lastUpdatedDateTime; private set
+            {
+                _lastUpdatedDateTime = value;
+                onPropertyChanged();
+            }
+        }
+        public string Name
+        {
+            get => _name; set
+            {
+                if(_name != value)
+                {
+                    _name = value;
+                    onPropertyChanged();
+                    NameChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event EventHandler<EventArgs> ValueUpdated;
+        public event EventHandler<EventArgs> ValueDestroyed;
+        public event EventHandler<EventArgs> NameChanged;
+
+        public SimpleVar(string name, string value, System.Collections.ObjectModel.ObservableCollection<SimpleVar> variables)
+        {
+            _centralizedVariablesCollection = variables;
+            Name = name;
+            StrValue = value;
+        }
+    }
+}
---
+++ VariablesManagementDemoApp/VariablesMgmtWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesManagementDemoApp
+{
+    public class VariablesMgmtWindowViewModel
+    {
+        private ObservableCollection<SimpleVar> variables = new ObservableCollection<SimpleVar>();
+
+        public ObservableCollection<SimpleVar> Variables { get => variables; set => variables = value; }
+
+        public VariablesMgmtWindowViewModel()
+        {
+            Variables.CollectionChanged += Variables_CollectionChanged;
+            Variables.Add(new SimpleVar("VariableA", "1", variables));
+            Variables.Add(new SimpleVar("VariableB", "2", variables));
+        }
+
+        private void Variables_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if(e.OldItems != null)
+            {
+                foreach (var item in e.OldItems)
+                {
+                    (item as SimpleVar).Destroy();
+                }
+            }
+        }
+
+        public void AddVariable(string name, string value)
+        {
+            if(Variables.Any(s => s.Name == name))
+            {
+                throw new Exception("Already a variable exists with the name");
+            }
+            else
+            {
+                Variables.Add(new SimpleVar(name, value, variables));
+            }
+        }
+
+        public void DeleteVariable(string name)
+        {
+            SimpleVar varToDelete = Variables.Where(s => s.Name == name).FirstOrDefault();
+            if(varToDelete != null)
+            {
+                Variables.Remove(varToDelete);
+            }
+        }
+    }
+}
